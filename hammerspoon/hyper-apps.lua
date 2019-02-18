6f844de307cf6fc16560ae1d1939e53e65f25a21
Mitchell Barron <hello@mitchellbarron.io>
2019-02-18 13:02:56 -0600

    Update hyper-apps.

---
+++ hammerspoon/hyper-apps.lua
@@ -0,0 +1,12 @@
+-- Default keybindings for launching apps in Hyper Mode
+--
+-- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
+-- this file, save it as `hyper-apps.lua`, and edit the table below to configure
+-- your preferred shortcuts.
+return {
+  { 'u', 'Vivaldi' },
+  { 'o', 'Chat' },
+  { 'i', 'iTerm' },
+  { 'y', 'Safari' },
+  { 'p', 'Messages' },
+}
